50960858161c0bb1ab69961553da9be97c9c0b2d
MinHyeo <p610222@naver.com>
2025-08-17 15:51:23 +0900

    Sloved 9465
    백준 9465 해결

---
+++ Baekjoon/Baekjoon/9465.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<vector<int>> sticker(2, vector<int>(n + 1, 0));
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> sticker[i][j + 1];
+			}
+		}
+
+		vector<vector<int>> dp(2, vector<int>(n + 1, 0));
+		dp[0][1] = sticker[0][1];
+		dp[1][1] = sticker[1][1];
+
+		for (int i = 2; i <= n; i++) {
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + sticker[0][i];
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + sticker[1][i];
+		}
+
+		cout << max(dp[0][n], dp[1][n]) << "\n";
+	}
+
+	return 0;
+}
