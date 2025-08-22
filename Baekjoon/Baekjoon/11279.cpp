9cde9cae4c7b7d5faf916c6156fcff740a7660e3
MinHyeo <p610222@naver.com>
2025-08-22 19:39:30 +0900

    Sloved 11279
    백준 11279 해결

---
+++ Baekjoon/Baekjoon/11279.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int> pq;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+
+		if (num == 0) {
+			if (!pq.empty()) {
+				cout<< pq.top() << "\n";
+				pq.pop();
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+		else {
+			pq.push(num);
+		}
+	}
+
+	return 0;
+}
