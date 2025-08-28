1954398437c316c9f6f981ecf842de0025df2e3e
MinHyeo <p610222@naver.com>
2025-08-28 21:25:47 +0900

    Sloved 1026
    백준 1026번 해결

---
+++ Baekjoon/Baekjoon/1026.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> a(n);
+	vector<int> b(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+	}
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<int>());
+
+	int result = 0;
+	for (int i = 0; i < n; i++) {
+		result += (a[i] * b[i]);
+	}
+	cout << result << "\n";
+
+	return 0;
+}
