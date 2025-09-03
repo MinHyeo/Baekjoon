c5a9ce9eee9b5cf10359e34250ce10faef8ab0d4
MinHyeo <p610222@naver.com>
2025-09-04 03:14:30 +0900

    Sloved 15656, 15663, 1809
    백준 15656, 15663, 1809 해결

---
+++ Baekjoon/Baekjoon/15656.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> numbers;
+vector<int> result;
+
+void BackTracking(int start, int depth) {
+	if (depth == m) {
+		for (int i = 0; i < m; i++) {
+			cout << result[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = start; i < n; i++) {
+		result.push_back(numbers[i]);
+		BackTracking(0, depth + 1);
+		result.pop_back();
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	numbers = vector<int>(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> numbers[i];
+	}
+	sort(numbers.begin(), numbers.end());
+
+	BackTracking(0, 0);
+
+	return 0;
+}
