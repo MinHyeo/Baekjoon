2d008a0d7cbda899838aead0a1075227a5fe241c
MinHyeo <p610222@naver.com>
2025-09-17 21:23:32 +0900

    Sloved 11758
    백준 11758 해결

---
+++ Baekjoon/Baekjoon/11758.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int x1, x2, x3, y1, y2, y3;
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+	long long ccw = ((long long)x2 - x1) * (y3 - y1) - ((long long)y2 - y1) * (x3 - x1);
+	if (ccw > 0) cout << 1 << "\n";
+	else if (ccw < 0) cout << -1 << "\n";
+	else cout << 0 << "\n";
+
+	return 0;
+}
